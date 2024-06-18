855f52ecccb9051f3e1920f42b58c79316a27965
Markus Finell <markus@marsapril.se>
2024-06-18 10:26:56 +0200

    fix

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 
-add_action('wp_enqueue_scripts', function () {
+function mofi_enqueue_scripts()
+{
     wp_enqueue_style('mofi-style', get_stylesheet_directory_uri() . '/style.css', [], '0.1.0');
-});
+}
+add_action('wp_enqueue_scripts', 'mofi_enqueue_scripts');
 
-add_action('admin_init', function () {
+function mofi_admin_init()
+{
     add_editor_style('style.css');
-});
+}
+add_action('admin_init', 'mofi_admin_init');
